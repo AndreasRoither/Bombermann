e0ee7745b3a77c404bd3748bd65fcbb3409c62ea
Unknown <andi.roither@hotmail.de>
2017-11-26 17:50:33 +0100

    Node JS Server
    Added socket.on create, for game server creation

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,17 +1,23 @@
+// load
+
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-const ConsoleColor = require('./colorcodes.js');
-
 var currentdate = new Date();
 var datetime = "Start time: " + currentdate.getDate() + "/" +
-    (currentdate.getMonth() + 1) + "/" +
-    currentdate.getFullYear() + " @ " +
-    currentdate.getHours() + ":" +
-    currentdate.getMinutes() + ":" +
-    currentdate.getSeconds();
+        (currentdate.getMonth() + 1) + "/" +
+        currentdate.getFullYear() + " @ " +
+        currentdate.getHours() + ":" +
+        currentdate.getMinutes() + ":" +
+        currentdate.getSeconds();
+
+// game vars
+var games = {},
+    avatars = ['Bomberman'];
+
+const ConsoleColor = require('./colorcodes.js');
 
 app.use(express.static(__dirname + '/node_modules'));
 
@@ -21,7 +27,37 @@ app.get('/', function(req, res, next) {
 
 io.on('connection', function(client) {
     console.log(ConsoleColor.Bright + ConsoleColor.FgGreen + '\nServer' + ConsoleColor.Reset);
-    console.log('\n\tClient connected...\n\tid: %s', client.id);
+    console.log('\n\tPlayer connected...\n\tid: %s', client.id);
+
+    // socket id for connecting player
+    var socketId = socket.id;
+    var gameID,
+        userName;
+
+    socket.on('create', function(id, name, avatar, matrix){
+        var player = {
+            id: socketId,
+            name: name,
+            avatar: avatar,
+            index: 0,
+            ready: false,
+            alive: true
+        };
+
+        games[i] = {
+            id: id,
+            players: [player],
+            matrix: matrix,
+            started: false,
+            created: Date.now()
+        };
+
+        gameID = id;
+        userName = name;
+
+        socket.join(id);
+        socket.emit('welcome', id, player);
+    });
 
     client.on('join', function(data) {
         console.log(ConsoleColor.Bright + ConsoleColor.FgCyan + '\nClient' + ConsoleColor.Reset);
