d49fa1bc819268782712222ef41041f28c4edd68
NeverfullD <nimmervoll.david@gmail.com>
2017-12-01 11:50:48 +0100

    Better Key Support
    added keyup listener and arrow keys

--- js/key_press.js
+++ js/key_press.js
@@ -1,32 +1,87 @@
 // Keyboard recognition
 
+var movLeft = 0;
+var movRight = 0;
+var movUp = 0;
+var movDown = 0;
+var layBomb = 0;
 
+window.addEventListener("keydown", function(event) {
 
-$(document).on('keypress', function(e) {
+  if(event.repeat)
+  return;
   if ( event.which == 13 ) { // Enter Key
-    //console.log( event.type + ": " +  event.which );
-    add_left_list('Enter pressed');
   }
   if ( event.which == 97 || event.which == 65) { // a || A Key
-    //console.log( event.type + ": " +  event.which );
-    add_left_list('a pressed');
+    movLeft = 1;
   }
   if ( event.which == 115 || event.which == 83) { // s || S Key
-    //console.log( event.type + ": " +  event.which );
-    add_left_list('s pressed');
+    movDown = 1;
   }
   if ( event.which == 100 || event.which == 68) { // d || D Key
-    //console.log( event.type + ": " +  event.which );
-    add_left_list('d pressed');
+    movRight = 1;
   }
   if ( event.which == 119 || event.which == 87) { // w || W Key
-    //console.log( event.type + ": " +  event.which );
-    add_left_list('w pressed');
+    movUp = 1;
   }
   if ( event.which == 32) { // space Key
-    //console.log( event.type + ": " +  event.which );
-    add_left_list('space pressed');
+    layBomb = 1;
   }
-  var close = document.getElementsByClassName("closebtn");
+ //Arrow Keys
+  if ( event.which == 37) { // left key
+    movLeft = 1;
+    event.preventDefault();
+  }
+  if ( event.which == 39) { // right key
+    movRight = 1;
+    event.preventDefault();
+  }
+  if ( event.which == 38) { // up key
+    movUp = 1;
+    event.preventDefault();
+  }
+  if ( event.which == 40) { // down key
+    movDown = 1;
+    event.preventDefault();
+  }
+
+  console.log( event.type + ": " +  event.keyCode ); //debug
 })
 
+window.addEventListener("keyup", function(event) {
+  if ( event.which == 13 ) { // Enter Key
+  }
+  if ( event.which == 97 || event.which == 65) { // a || A Key
+    movLeft = 0;
+  }
+  if ( event.which == 115 || event.which == 83) { // s || S Key
+    movDown = 0;
+  }
+  if ( event.which == 100 || event.which == 68) { // d || D Key
+    movRight = 0;
+  }
+  if ( event.which == 119 || event.which == 87) { // w || W Key
+    movUp = 0;
+  }
+  if ( event.which == 32) { // space Key
+    layBomb = 0;
+  }
+ //Arrow Keys
+  if ( event.which == 37) { // left key
+    movLeft = 0;
+    event.preventDefault();
+  }
+  if ( event.which == 39) { // right key
+    movRight = 0;
+    event.preventDefault();
+  }
+  if ( event.which == 38) { // up key
+    movUp = 0;
+    event.preventDefault();
+  }
+  if ( event.which == 40) { // down key
+    movDown = 0;
+    event.preventDefault();
+  }
+  console.log( event.type + ": " +  event.keyCode ); //debug
+})
