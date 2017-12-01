dcc685d61d5de8f29d2955097cc3935fc1fc6f91
Andreas Roither <andi.roither@hotmail.de>
2017-12-01 11:55:37 +0100

    Node JS Server, Client js
    Added Server emits
    Added event listeners client sided
    Added functions in functions.js

---
+++ js/io.js
@@ -0,0 +1,59 @@
+// connect
+
+var socket = io.connect('https://localhost:4200/');
+
+// event listeners
+
+socket.on('welcome', function(id, playerInfo) {
+
+});
+
+
+socket.on('game-not-found', function(id, playerInfo) {
+
+});
+
+socket.on('game-started', function(id, playerInfo) {
+
+});
+
+socket.on('joined', function(id, playerInfo) {
+
+});
+
+socket.on('ready', function(id, playerInfo) {
+
+});
+
+socket.on('start', function(id, playerInfo) {
+
+});
+
+socket.on('stop', function(id, playerInfo) {
+
+});
+
+
+socket.on('move', function(id, playerInfo) {
+
+});
+
+socket.on('bomb', function(id, playerInfo) {
+
+});
+
+socket.on('death', function(id, playerInfo) {
+
+});
+
+socket.on('win', function(id, playerInfo) {
+
+});
+
+socket.on('left', function(id, playerInfo) {
+
+});
+
+socket.on('player-message', function(id, playerInfo) {
+
+});
