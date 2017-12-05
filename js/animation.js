a46160380ed3e335597fdbfdd90401d0473d6db5
Unknown <andi.roither@hotmail.de>
2017-12-05 22:12:25 +0100

    Animations.js
    Cleaned up and commented code
    Removed function outside of objects and added them to the objects that neeeded them
    
    Issues:
    Draw x and y pos is not the same pos we want to check if a player can move to, so we could add a drawPosition and a pos to check for boundaries

--- js/animation.js
+++ js/animation.js
@@ -1,14 +1,91 @@
-var myGamePiece;
+/********************/
+/*     Functions    */
+/********************/
 
 function startGame() {
   myGameArea.start();
   myBackground = new background(myGameArea.context, 35);
-  myGamePiece = new player(myGameArea.context, 70, 35, 0.5, 7);
-  //myGamePiece = new component(30, 30, "red", 10, 120);
+  myGamePiece = new player(myGameArea.context, 36, 36, 0.5, 4);
+
+  // TODO: start game loop here instead of setinterval from mygamearea
 }
 
+function updateGameArea() {
+  myGameArea.clear();
+
+  myBackground.update();
+  myGamePiece.resetSpeed();
+
+  if (!checkInput()) {
+    myGamePiece.imageCounter = 0;
+  }
+
+  if (myGamePiece.possibleToMove(myBackground.tileSize))
+    myGamePiece.newPos();
+  myGamePiece.update();
+
+  myGameArea.context.rect(myGamePiece.pos.posX, myGamePiece.pos.posY, 1, 1);
+  myGameArea.context.strokeStyle="blue";
+  myGameArea.context.stroke();
+}
+
+/* checks if input is one of the desired keys and
+ * updates the direction + image counter
+ * also checks if players moves diagonally */
+function checkInput() {
+  var moved = false;
+  var movedDiagonally = false;
+
+  if (myGameArea.keys && myGameArea.keys[37]) {
+    myGamePiece.currentDirection = directions.left;
+    myGamePiece.speed.speedX = -myGamePiece.walkSpeed;
+    moved = true;
+  }
+  if (myGameArea.keys && myGameArea.keys[39]) {
+    myGamePiece.currentDirection = directions.right;
+    myGamePiece.speed.speedX = myGamePiece.walkSpeed;
+    moved = true;
+  }
+  if (myGameArea.keys && myGameArea.keys[38]) {
+    myGamePiece.currentDirection = directions.up;
+    myGamePiece.speed.speedY = -myGamePiece.walkSpeed;
+    if (moved)
+      movedDiagonally = true;
+    else
+      moved = true;
+  }
+  if (myGameArea.keys && myGameArea.keys[40]) {
+    myGamePiece.currentDirection = directions.down;
+    myGamePiece.speed.speedY = myGamePiece.walkSpeed;
+    if (moved)
+      movedDiagonally = true;
+    else
+      moved = true;
+  }
+
+  // move speed is to high if moved diagonally, so we reset it
+  if (movedDiagonally) {
+    myGamePiece.speed.speedX = myGamePiece.speed.speedX / myGamePiece.diagonalMoveDivisior;
+    myGamePiece.speed.speedY = myGamePiece.speed.speedY / myGamePiece.diagonalMoveDivisior;
+  }
+
+  // update image counter and return true if moved
+  if (moved) {
+    myGamePiece.updateImageCounter();
+    return true;
+  }
+  else
+    return false;
+}
+
+/********************/
+/*   Declarations   */
+/********************/
+var myGamePiece;
+
+/* contains all player image paths */
 var playerImages = {
-  tileCount : 8,
+  tileCount: 8,
   frontImgs: [
     "./img/Bomberman/Front/Bman_F_f00.png",
     "./img/Bomberman/Front/Bman_F_f01.png",
@@ -51,8 +128,9 @@ var playerImages = {
   ]
 };
 
+/* contains all tile image paths */
 var tileImages = {
-  tileCount : 3,
+  tileCount: 3,
   tiles: [
     './img/Blocks/BackgroundTile.png',
     './img/Blocks/SolidBlock.png',
@@ -60,7 +138,8 @@ var tileImages = {
   ]
 };
 
-// Image factory
+/* Image factory to create an image
+ * returns image; */
 var createImage = function (src, title) {
   var img = new Image();
   img.src = src;
@@ -69,6 +148,7 @@ var createImage = function (src, title) {
   return img;
 };
 
+/* Game Area (Canvas) */
 var myGameArea = {
   canvas: document.getElementById("gameCanvas"),
   start: function () {
@@ -76,23 +156,24 @@ var myGameArea = {
     this.canvas.height = 500;
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    this.interval = setInterval(updateGameArea, 33);
+    this.interval = setInterval(updateGameArea, 100);
     this.keys = [];
 
     // Event listener
     window.addEventListener('keydown', function (e) {
       myGameArea.keys = (myGameArea.keys || []);
       myGameArea.keys[e.keyCode] = (e.type == "keydown");
-  })
-  window.addEventListener('keyup', function (e) {
+    })
+    window.addEventListener('keyup', function (e) {
       myGameArea.keys[e.keyCode] = (e.type == "keydown");
-  })
+    })
   },
   clear: function () {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
 };
 
+/* direction enum to distinguish directions */
 var directions = {
   left: 1,
   right: 2,
@@ -100,13 +181,18 @@ var directions = {
   down: 4
 };
 
-function player(context, x, y, playerSize, walkSpeed) {
-  this.gamearea = myGameArea;
+/********************/
+/*     Objects      */
+/********************/
+
+/* Player Object 
+ * Contains vars and funcitons to move and draw the player + check functions*/
+function player(context, x, y, playerSizeMultiplier, walkSpeed) {
   this.imageCounter = 0;
   this.currentDirection = directions.down;
   this.oldDirection = this.currentDirection;
   this.ctx = context;
-  this.playerSize = playerSize;
+  this.playerSizeMultiplier = playerSizeMultiplier;
   this.walkSpeed = walkSpeed;
   this.diagonalMoveDivisior = 1.4;
 
@@ -125,11 +211,17 @@ function player(context, x, y, playerSize, walkSpeed) {
     posY: y
   };
 
+  this.drawPosition = {
+    posX: x,
+    posY: y
+  }
+
   this.front = [];
   this.back = [];
   this.left = [];
   this.right = [];
 
+  // crreate and initialize images from the image paths and push it into the arrays
   for (var i = 0; i < playerImages.tileCount; i++) {
     this.front.push(createImage(playerImages.frontImgs[i], "Bomberman Front"));
     this.back.push(createImage(playerImages.backImgs[i], "Bomberman Back"));
@@ -137,197 +229,147 @@ function player(context, x, y, playerSize, walkSpeed) {
     this.right.push(createImage(playerImages.rightImgs[i], "Bomberman Right"));
   }
 
-  // Functions
+  // draws player according to the current looking direction
   this.update = function () {
     switch (this.currentDirection) {
       case directions.left:
-        draw_player(this.left[this.imageCounter], this.pos, this.dimensions, this.playerSize);
+        this.drawPlayer(this.left[this.imageCounter], this.pos, this.dimensions, this.playerSizeMultiplier);
         break;
       case directions.right:
-        draw_player(this.right[this.imageCounter], this.pos, this.dimensions, this.playerSize);
+        this.drawPlayer(this.right[this.imageCounter], this.pos, this.dimensions, this.playerSizeMultiplier);
         break;
       case directions.up:
-        draw_player(this.back[this.imageCounter], this.pos, this.dimensions, this.playerSize);
+        this.drawPlayer(this.back[this.imageCounter], this.pos, this.dimensions, this.playerSizeMultiplier);
         break;
       case directions.down:
-        draw_player(this.front[this.imageCounter], this.pos, this.dimensions, this.playerSize);
+        this.drawPlayer(this.front[this.imageCounter], this.pos, this.dimensions, this.playerSizeMultiplier);
         break;
     }
   };
 
+  /* updates image counter
+   * determines which frame of the player should be drawn*/
   this.updateImageCounter = function () {
-    if (this.currentDirection != this.oldDirection){
+    if (this.currentDirection != this.oldDirection) {
       imageCounter = 0;
       this.oldDirection = this.currentDirection;
     }
 
     if (this.imageCounter < 7) {
       this.imageCounter = this.imageCounter + 1;
-    }
-    else {
+    } else {
       this.imageCounter = 0;
     }
   };
 
+  // set new position of the player
   this.newPos = function () {
     this.pos.posX += this.speed.speedX;
     this.pos.posY += this.speed.speedY;
+    this.drawPosition.posX += this.speed.speedX;
+    this.drawPosition.posY += this.speed.speedY;
   };
 
+  // resets the speed; else player won't stop moving ~
   this.resetSpeed = function () {
     this.speed.speedX = 0;
     this.speed.speedY = 0;
   };
 
+  // check if it is possible to move
   this.possibleToMove = function (tileSize) {
     var newPos = {
-      posX : (this.pos.posX + this.speed.speedX),
-      posY : (this.pos.posY + this.speed.speedY)
+      posX: (this.pos.posX + this.speed.speedX),
+      posY: (this.pos.posY + this.speed.speedY)
     };
-    console.log(this.pos.posX);
-    if (possible_move(this.pos, newPos, tileSize))
+
+    if (this.possible_move(this.pos, newPos, tileSize))
       return true;
     else
       return false;
   };
+
+  //checks if a single Point can be moved
+  this.possible_move = function (pos, newPos, tileSize) {
+    console.log ("Move Check| x: " + pos.posX + " y: " + pos.posY);
+    if (myBackground.map[Math.trunc((pos.posY + newPos.posY) / tileSize)][Math.trunc((pos.posX + newPos.posX) / tileSize)] == 0) {
+      console.log("Tile: " + myBackground.map[Math.trunc((pos.posY + newPos.posY) / tileSize)][Math.trunc((pos.posX + newPos.posX) / tileSize)]);
+      console.log("X: " + Math.trunc((pos.posY + newPos.posY) / tileSize) + "Y: " + Math.trunc((pos.posX + newPos.posX) / tileSize));
+      return true;
+    }
+    console.log("Tile: " + myBackground.map[Math.trunc((pos.posY + newPos.posY) / tileSize)][Math.trunc((pos.posX + newPos.posX) / tileSize)]);
+    console.log("X: " + Math.trunc((pos.posY + newPos.posY) / tileSize) + "Y: " + Math.trunc((pos.posX + newPos.posX) / tileSize));
+    return false;
+  };
+
+  // draws the player
+  this.drawPlayer = function (img, pos, size, playerSizeMultiplier) {
+    myGameArea.context.drawImage(img, pos.posX, pos.posY, size.width * playerSizeMultiplier, size.height * playerSizeMultiplier);
+  };
 }
 
+/* Background Object 
+ * contains vars and functions to draw the background*/
 function background(context, tileSize) {
   this.tiles = [];
   this.tileSize = tileSize;
+  this.height = 0;
+  this.width = 0;
+  this.ctx = context;
+
   this.dimensions = {
     width: 13,
     height: 19
   };
-  this.height = 0;
-  this.width = 0;
-  this.ctx = context;
+  
   this.map = [
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1],
-    [1,0,0,2,2,2,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,1,2,1,2,1,0,1,0,1,0,1,0,1,0,1,0,1],
-    [1,0,0,2,2,2,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1],
-    [1,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1],
-    [1,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1],
-    [1,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
-    ];
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 2, 1, 2, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  ];
 
+  // create new images and push them into the tiles array
   for (var i = 0; i < tileImages.tileCount; i++) {
     this.tiles.push(createImage(tileImages.tiles[i], "Tile"));
   }
 
+  // update function draws the background
   this.update = function () {
-    draw_background(this.ctx, this.map, this.dimensions, this.tiles, this.tileSize);
-  }
-}
+    this.draw_background(this.ctx, this.map, this.dimensions, this.tiles, this.tileSize);
+  };
 
-
-function updateGameArea() {
-  myGameArea.clear();
-
-  myBackground.update();
-  myGamePiece.resetSpeed();
-
-  if (!checkInput()) {
-    myGamePiece.imageCounter = 0;
-  }
-
-  if (myGamePiece.possibleToMove(myBackground.tileSize))
-    myGamePiece.newPos();
-  myGamePiece.update();
-}
-
-function checkInput () {
-  var moved = false;
-  var movedDiagonally = false;
-
-  if (myGameArea.keys && myGameArea.keys[37]) {
-    myGamePiece.currentDirection = directions.left;
-    myGamePiece.updateImageCounter();
-    myGamePiece.speed.speedX = -myGamePiece.walkSpeed;
-    moved = true;
-  }
-  if (myGameArea.keys && myGameArea.keys[39]) {
-    myGamePiece.currentDirection = directions.right;
-    myGamePiece.updateImageCounter();
-    myGamePiece.speed.speedX = myGamePiece.walkSpeed;
-    moved = true;
-  }
-  if (myGameArea.keys && myGameArea.keys[38]) {
-    myGamePiece.currentDirection = directions.up;
-    myGamePiece.updateImageCounter();
-    myGamePiece.speed.speedY = -myGamePiece.walkSpeed;
-    if (moved)
-      movedDiagonally = true;
-    else 
-      moved = true;
-  }
-  if (myGameArea.keys && myGameArea.keys[40]) {
-    myGamePiece.currentDirection = directions.down;
-    myGamePiece.updateImageCounter();
-    myGamePiece.speed.speedY = myGamePiece.walkSpeed;
-    if (moved)
-      movedDiagonally = true;
-    else 
-      moved = true;
-  }
-
-  if (movedDiagonally)
-  {
-    console.log("moved diagonally");
-    myGamePiece.speed.speedX = myGamePiece.speed.speedX / myGamePiece.diagonalMoveDivisior;
-    myGamePiece.speed.speedY = myGamePiece.speed.speedY / myGamePiece.diagonalMoveDivisior;
-  }
-
-  if (moved)
-    return true;
-  else
-    return false;
-}
-
-function draw_player(img, pos, size, playerSize) {
-  myGameArea.context.drawImage(
-    img,
-    pos.posX * playerSize,
-    pos.posY * playerSize,
-    size.width * playerSize,
-    size.height * playerSize
-  );
-}
-
-function draw_background (ctx, background, dimensions, tiles, tileSize) {
-  for (var y = 0; y < dimensions.width; ++y) {
-    for (var x = 0; x < dimensions.height; ++x) {
-
-      switch(background[y][x]) {
-        case 0: //background
-          draw_block(ctx, tiles[0], x, y, tileSize);
-          break;
-        case 1: //solid
-          draw_block(ctx, tiles[1], x, y, tileSize);
-          break;
-        case 2: //explodable
-          draw_block(ctx, tiles[2], x, y, tileSize);
-          break;
+  // draw_background draws background according to the matrix (this.map)
+  this.draw_background = function (ctx, background, dimensions, tiles, tileSize) {
+    for (var y = 0; y < dimensions.width; ++y) {
+      for (var x = 0; x < dimensions.height; ++x) {
+        switch (background[y][x]) {
+          case 0: //background
+            this.draw_block(ctx, tiles[0], x, y, tileSize);
+            break;
+          case 1: //solid
+            this.draw_block(ctx, tiles[1], x, y, tileSize);
+            break;
+          case 2: //explodable
+            this.draw_block(ctx, tiles[2], x, y, tileSize);
+            break;
+        }
       }
     }
-  }
+  };
+
+  // draws a block to the context
+  this.draw_block = function (ctx, img, x, y, tileSize) {
+    ctx.drawImage(img, tileSize * x, tileSize * y, tileSize, tileSize);
+  };
 }
 
-function draw_block (ctx, img, x, y, tileSize) {
-  ctx.drawImage(img, tileSize*x, tileSize*y, tileSize, tileSize);
-}
-
-//checks if a single Point can be moved
-function possible_move(pos, newPos, tileSize) {
-  if (myBackground.map[Math.trunc((pos.posY + newPos.posY)/tileSize)][Math.trunc((pos.posX + newPos.posX)/tileSize)] == 0) {
-    return true;
-  }
-  return false;
-}
